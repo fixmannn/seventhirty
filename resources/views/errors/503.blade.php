ba4303099ed7cd554f1afe6d4a293a707c15c2fe
fixmannn <u1276051@srv118.niagahoster.com>
2021-07-22 23:23:01 +0700

    publish customize error pages

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,6 @@
+@extends('errors::minimal')
+
+@section('title', __('Website Under Maintenance'))
+@section('code', '503')
+@section('message', __('This website is under maintenance, please 
+comeback soon :)'))
