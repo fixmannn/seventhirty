c973b7365bf6cc62622aeeb036c8d77402a44f0a
fixmannn <79675945+fixmannn@users.noreply.github.com>
2021-07-23 09:42:51 +0700

    changing asset source

--- public/js/products.js
+++ public/js/products.js
@@ -7,33 +7,35 @@ var page = path.split("/products/").pop();
 let product = page.split("/").shift();
 
 catalogs.forEach((val, index) => {
-    val.addEventListener('mouseover', function() {
-        val.src = "http://localhost:8888/seven-thirty/public/img/" + catPics[index] + "-zoomed.jpg";
+    val.addEventListener('mouseover', function () {
+        val.src = <? php echo asset('img/') ?> + catPics[index] + "-zoomed.jpg";
         val.classList.add('fadeIn');
     })
 });
 
 catalogs.forEach((val, index) => {
-    val.addEventListener('mouseout', function() {
-        val.src = "http://localhost:8888/seven-thirty/public/img/" + catPics[index] + ".jpg";
+    val.addEventListener('mouseout', function () {
+        val.src = <? php echo asset('img/') ?> + catPics[index] + ".jpg";
         val.classList.remove('fadeIn');
     })
 });
 
-thumb2.addEventListener('click', function() {
-    previewImg.src = "http://localhost:8888/seven-thirty/public/img/" + product + "-zoomed.jpg";
+thumb2.addEventListener('click', function () {
+    previewImg.src = <? php echo asset('img/') ?> + product + "-zoomed.jpg";
     event.preventDefault();
     previewImg.classList.add('fadeIn');
-    setTimeout(function(){ 
-    previewImg.classList.remove('fadeIn'); }, 200);
+    setTimeout(function () {
+        previewImg.classList.remove('fadeIn');
+    }, 200);
 });
 
-thumb1.addEventListener('click', function() {
-    previewImg.src = "http://localhost:8888/seven-thirty/public/img/" + product + ".jpg";
+thumb1.addEventListener('click', function () {
+    previewImg.src = <? php echo asset('img/') ?> + product + ".jpg";
     event.preventDefault();
     previewImg.classList.add('fadeIn');
-    setTimeout(function(){ 
-    previewImg.classList.remove('fadeIn')}, 200);
+    setTimeout(function () {
+        previewImg.classList.remove('fadeIn')
+    }, 200);
 });
 
 
@@ -44,23 +46,23 @@ const available = document.querySelector('.stock-available');
 const notAvailable = document.querySelector('.stock-na');
 const cartBtn = document.querySelector('#cart-btn');
 
-radioBtn.forEach(function(size) {
-    if(size.checked = false) {
+radioBtn.forEach(function (size) {
+    if (size.checked = false) {
         message.style.display = "block";
     } else {
-        size.addEventListener('click', function() {
-        if(size.ariaLabel == 0) {
-            notAvailable.style.display = "block";
-            available.style.display = "none";
-            cartBtn.setAttribute("disabled", "true");
-        } else {
-            available.style.display = "block";
-            notAvailable.style.display = "none";
-            cartBtn.removeAttribute("disabled");
-        }
+        size.addEventListener('click', function () {
+            if (size.ariaLabel == 0) {
+                notAvailable.style.display = "block";
+                available.style.display = "none";
+                cartBtn.setAttribute("disabled", "true");
+            } else {
+                available.style.display = "block";
+                notAvailable.style.display = "none";
+                cartBtn.removeAttribute("disabled");
+            }
         });
     }
-    
+
 
 });
 
@@ -72,9 +74,9 @@ const price = document.getElementById('number');
 const priceInner2 = document.getElementById('number2').innerText;
 const price2 = document.getElementById('number2');
 
-console.log(typeof(priceInner));
+console.log(typeof (priceInner));
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     price2.innerHTML = parseFloat(priceInner2).toLocaleString('id');
     price.innerHTML = parseFloat(priceInner).toLocaleString('id');
 });
