e265ac68fda8335e46aede3231eefec602e15c8b
fixmannn <79675945+fixmannn@users.noreply.github.com>
2021-07-27 10:21:25 +0700

    add some changes

--- resources/views/account/details.blade.php
+++ resources/views/account/details.blade.php
@@ -12,23 +12,40 @@
 <div class="details">
     <div class="row">
         <div class="col-xl-3">
-            <h3 class="font-weight-bold title">Account Details</h3>
+            <h5 class="font-weight-bold title">Account Details</h5>
             <div class="d-flex justify-content-between btn mt-3">
                 <p class="text-muted font-weight-bold active">Edit Profile</p>
                 <span class="iconify text-muted active" data-icon="ic:baseline-navigate-next" data-inline="false"></span>
             </div>
             <div class="d-flex justify-content-between btn mt-1">
-                <p class="text-muted font-weight-bold">Change Password</p>
-                <span class="iconify text-muted" data-icon="ic:baseline-navigate-next" data-inline="false"></span>
+                <p class="text-black-50 font-weight-bold">Change Password</p>
+                <span class="iconify text-black-50" data-icon="ic:baseline-navigate-next" data-inline="false"></span>
             </div>
             <div class="d-flex justify-content-between btn mt-1">
-                <p class="text-muted font-weight-bold">Orders</p>
-                <span class="iconify text-muted" data-icon="ic:baseline-navigate-next" data-inline="false"></span>
+                <p class="text-black-50 font-weight-bold">Orders</p>
+                <span class="iconify text-black-50" data-icon="ic:baseline-navigate-next" data-inline="false"></span>
             </div>
         </div>
         <div class="col-xl-9">
-          <h3 class="font-weight-bold title">Edit Profile</h3>
-          
+          <h5 class="font-weight-bold title">Edit Profile</h5>
+          <form>
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="first-name">Nama Depan</label>
+                <input type="text" class="form-control" id="first-name" name="first_name">
+              </div>
+              <div class="form-group col-md-6">
+                <label for="last-name">Nama Belakang</label>
+                <input type="text" class="form-control" id="last-name" name="last_name">
+              </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md">
+                    <label for="address">Alamat</label>
+                    <input type="text" class="form-control" id="address" name="address">
+                </div>
+            </div>
+          </form>
         </div>
     </div>
 </div>
