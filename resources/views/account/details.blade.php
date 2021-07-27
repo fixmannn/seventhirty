24416080b9bfa286e570e2d16ca5b4af9a6e91c2
fixmannn <79675945+fixmannn@users.noreply.github.com>
2021-07-27 14:42:35 +0700

    fix jquery

--- resources/views/account/details.blade.php
+++ resources/views/account/details.blade.php
@@ -89,7 +89,7 @@
 @include('layouts/footer')
 
 <script>
-    var dataForm = $(elem).data('form');
+    var dataForm = $('input').data('form');
 
     $('.edit-form').click(function() {
         dataForm.prop('readonly', false);
