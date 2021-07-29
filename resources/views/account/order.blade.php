7e93a9ad38d7c069de6795469d9e9f103dcc0d04
fixmannn <79675945+fixmannn@users.noreply.github.com>
2021-07-29 08:05:29 +0700

    fix some changes

--- resources/views/account/order.blade.php
+++ resources/views/account/order.blade.php
@@ -50,8 +50,7 @@
           <table class="mt-3 table table-borderless">
             <tr>
               <td><img src="{{ asset('/img/anxiety.jpg') }}" alt="" width="90px" class="product-img">
-                <span class="font-weight-bold ml-3">Anxiety Disorder - T Shirt <br>
-                  <p class="text-muted">XL - 1 pcs</p></span>
+                <span class="font-weight-bold ml-3">Anxiety Disorder - T Shirt <br> XL - 1 pcs</span>
               </td>
               <td style="vertical-align: middle;"><span class="font-weight-bold d-flex align-items-center">Rp. 180.000,-</span></td>
             </tr>
