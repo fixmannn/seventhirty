bd5af7e665d92310890e184c83010861d1276fea
fixmannn <79675945+fixmannn@users.noreply.github.com>
2021-07-29 11:19:23 +0700

    fix some changes

--- resources/views/account/order.blade.php
+++ resources/views/account/order.blade.php
@@ -53,7 +53,7 @@
               </td>
               <td style="vertical-align: middle; padding-left: 0;" class="ml-1" width="65%"><span class="font-weight-bold">Anxiety Disorder</span><br>
               <span class="text-muted">XL - 1 pcs</span></td>
-              <td style="vertical-align: middle; padding-right: 0; text-align: center;" width="30%" class="font-weight-bold"><span>Rp. 180.000,-</span></td>
+              <td style="vertical-align: middle; padding-right: 0;" width="30%" class="font-weight-bold"><span>Rp. 180.000,-</span></td>
             </tr>
           </table>
         </div>
