a7db3cde8654070a5bc46ef7a8e9ded2018ef468
fixmannn <79675945+fixmannn@users.noreply.github.com>
2021-08-04 10:49:04 +0700

    fix some changes

--- resources/views/layouts/pages.blade.php
+++ resources/views/layouts/pages.blade.php
@@ -12,6 +12,7 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
   {{-- Data Table plugin --}}
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap4.min.css">
   
   @section('orders')
   <link rel="stylesheet" href="{{asset('css/orders.css')}}">
