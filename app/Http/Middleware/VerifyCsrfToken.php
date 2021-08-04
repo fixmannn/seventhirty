d4bed0ea1d3703b55d3bcab6c607a207c6a48785
fixmannn <79675945+fixmannn@users.noreply.github.com>
2021-08-04 16:49:58 +0700

    fix some changes

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,7 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
         '/products/*',
-        '/add-to-cart/{id}'
+        '/add-to-cart/{id}',
+        '/payment/'
     ];
 }
