c5d99d086d1df3b341043a5eadf08651471855a7
fixmannn <79675945+fixmannn@users.noreply.github.com>
2021-08-04 09:36:57 +0700

    fix some changes

--- resources/views/layouts/pages.blade.php
+++ resources/views/layouts/pages.blade.php
@@ -10,8 +10,8 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
   {{-- Data Table plugin --}}
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/datatables.min.css"/>
-
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.css">
+  
   <!-- Internal CSS -->
   @yield('shopcss')
   @yield('gallerycss')
