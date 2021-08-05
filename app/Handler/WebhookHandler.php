aabc8e1032b87580361e6e3f476b56bb08c0d4e5
fixmannn <79675945+fixmannn@users.noreply.github.com>
2021-08-05 11:38:48 +0700

    add webhook handler

---
+++ app/Handler/WebhookHandler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Handler;
+
+use Spatie/WebhookClient/ProcessWebhookJob;
+
+class WebhookHandler extends ProcessWebhookJob
+{
+  public function handle()
+  {
+    logger($this->webhookCall);
+  }
+}
