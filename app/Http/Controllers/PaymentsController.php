7408932848632bd9b7539431158f1255566ee312
fixmannn <79675945+fixmannn@users.noreply.github.com>
2021-08-07 16:09:44 +0700

    fix some changes

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -115,7 +115,7 @@ class PaymentsController extends Controller
 
         session()->put('success', $success);
 
-        return $this->status();
+        return response('ok', 200);
     }
 
     public function home()
