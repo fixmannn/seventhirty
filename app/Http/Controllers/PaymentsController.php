f6f4a9e9e9bd2b644842cca7954698d8b2c30442
fixmannn <79675945+fixmannn@users.noreply.github.com>
2021-08-08 11:20:51 +0700

    fix some changes

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -18,7 +18,7 @@ class PaymentsController extends Controller
         $time = time();
 
         if (isset($success)) {
-            return redirect()->route('payment-success', [PaymentsController::class, 'status']);
+            $this->status();
         } else {
             if ($expiration['timestamp'] > $time) {
                 return view('checkout.payment');
