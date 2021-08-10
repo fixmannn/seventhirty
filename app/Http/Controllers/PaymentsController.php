bc74f4f5595dbc7b64ee20cb7da39ab5a71d2adc
fixmannn <79675945+fixmannn@users.noreply.github.com>
2021-08-10 07:22:13 +0700

    fix some changes

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -133,9 +133,9 @@ class PaymentsController extends Controller
 
     public function check(Request $request)
     {
-        $url = 'https://seventhirty-id.com/payment-check';
-        $data = json_decode(file_get_contents($url), true);
-        // $paid = $request->all();
+        // $url = 'https://seventhirty-id.com/payment-check';
+        // $data = json_decode(file_get_contents($url), true);
+        $data = $request->all();
 
         // session()->put('paid', $paid);
 
