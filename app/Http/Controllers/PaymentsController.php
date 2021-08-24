0aa902404ee7ec72025927ac7776060e37ca6777
fixmannn <79675945+fixmannn@users.noreply.github.com>
2021-08-24 20:05:13 +0700

    fix some changes

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -149,8 +149,7 @@ class PaymentsController extends Controller
         $details = OrderDetail::where('order_number', $order_number)->get();
 
         foreach($details as $products) {
-            $product[] = Product::where('id', $products['product_id'])->get();
-            return $product;
+            return $products['product_id'];
         }
 
     }
