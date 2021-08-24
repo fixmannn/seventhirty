4abd6dc2b46d4c13ba2505af3e903846d03458c4
fixmannn <79675945+fixmannn@users.noreply.github.com>
2021-08-24 20:27:06 +0700

    fix some changes

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -50,7 +50,7 @@ class PaymentsController extends Controller
         $details = OrderDetail::where('order_number', $order_number)->get();
 
         foreach($details as $products) {
-            $product = Product::where('id', $products['product_id'])->get();
+            $product = Product::where('id', $products['product_id'])->first();
             dump($product);    
         }
     }
