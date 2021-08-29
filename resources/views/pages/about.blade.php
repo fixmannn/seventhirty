7c7db7254f6737931ee54449ade62df4b986ae81
fixmannn <79675945+fixmannn@users.noreply.github.com>
2021-08-29 08:03:43 +0700

    fix some css

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -11,8 +11,8 @@
 <div class="about">
   <div class="row mt-4 mb-5">
     <div class="col">
-      <div class="container img-container">
-        <img src="{{ asset('/img/page-down.png') }}" alt="" class="img-fluid mx-auto d-block">
+      <div class="container">
+        <img src="{{ asset('/img/page-down.png') }}" alt="" class="img-fluid mx-auto d-block img-content">
       </div>
     </div>
   </div>
